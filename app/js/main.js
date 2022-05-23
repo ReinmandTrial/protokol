6bc2b4d5b840ed457a5c3af78f8be49968833151
Nik Gor <nikcosmos@gmail.com>
2022-05-23 16:32:30 +0300

    fix main class

--- app/js/main.js
+++ app/js/main.js
@@ -308,17 +308,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
 //QUESTION========================================================================================================================================================
 
-const requestBtnEl = document.querySelector('.lawyers-found__request');
-const cancelBtnEl = document.querySelector('.lawyers-found__cancel');
+// const requestBtnEl = document.querySelector('.lawyers-found__request');
+// const cancelBtnEl = document.querySelector('.lawyers-found__cancel');
 
-const lawyersFoundEl = document.querySelector('.lawyers-found');
+// const lawyersFoundEl = document.querySelector('.lawyers-found');
 
-requestBtnEl.addEventListener('click', onRequestBtnClick);
-cancelBtnEl.addEventListener('click', onСancelBtnClick);
+// requestBtnEl.addEventListener('click', onRequestBtnClick);
+// cancelBtnEl.addEventListener('click', onСancelBtnClick);
 
-function onRequestBtnClick() {
-   lawyersFoundEl.classList.add('active-quest');
-}
-function onСancelBtnClick() {
-   lawyersFoundEl.classList.remove('active-quest');
-}
+// function onRequestBtnClick() {
+//    lawyersFoundEl.classList.add('active-quest');
+// }
+// function onСancelBtnClick() {
+//    lawyersFoundEl.classList.remove('active-quest');
+// }
