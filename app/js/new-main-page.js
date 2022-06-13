587b1d54ca24e4b9193ba066336a9d26cd1ac0a3
Nik Gor <nikcosmos@gmail.com>
2022-06-13 19:20:10 +0300

    new main

---
+++ app/js/new-main-page.js
@@ -0,0 +1,8 @@
+const listBtnEl = document.querySelectorAll('.faq-spoiler__btn');
+
+listBtnEl.forEach((btn) => btn.addEventListener('click', onToggleClickBtn));
+
+function onToggleClickBtn(e) {
+   const currentItem = e.target.closest('.faq-spoiler__item');
+   currentItem.classList.toggle('active');
+}
