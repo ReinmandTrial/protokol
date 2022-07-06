a850006433b41141026d8333d00d776637395d05
NikCosmos <nikcosmos@gmail.com>
2022-07-06 22:55:01 +0300

    service-page DONE

--- gulpfile.js
+++ gulpfile.js
@@ -9,73 +9,62 @@ const imagemin = require('gulp-imagemin');
 const del = require('del');
 
 function browsersync() {
-  browserSync.init({
-    server: {
-      baseDir: 'app/'
-    }
-  });
+   browserSync.init({
+      server: {
+         baseDir: 'app/',
+      },
+   });
 }
 
 function cleanDist() {
-  return del('dist')
+   return del('dist');
 }
 
 function images() {
-  return src('app/images/**/*')
-    .pipe(imagemin(
-      [
-        imagemin.gifsicle({ interlaced: true }),
-        imagemin.mozjpeg({ quality: 75, progressive: true }),
-        imagemin.optipng({ optimizationLevel: 5 }),
-        imagemin.svgo({
-          plugins: [
-            { removeViewBox: true },
-            { cleanupIDs: false }
-          ]
-        })
-      ]
-    ))
-    .pipe(dest('dist/images'))
+   return src('app/images/**/*')
+      .pipe(
+         imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.mozjpeg({ quality: 75, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+               plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+            }),
+         ])
+      )
+      .pipe(dest('dist/images'));
 }
 
 function scripts() {
-  return src([
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/swiper/swiper-bundle.min.js',
-    'app/js/main.js'
-  ])
-    .pipe(concat('main.min.js'))
-    .pipe(uglify())
-    .pipe(dest('app/js'))
-    .pipe(browserSync.stream())
+   return src(['node_modules/jquery/dist/jquery.js', 'node_modules/swiper/swiper-bundle.min.js', 'app/js/main.js'])
+      .pipe(concat('main.min.js'))
+      .pipe(uglify())
+      .pipe(dest('app/js'))
+      .pipe(browserSync.stream());
 }
 
 function styles() {
-  return src('app/scss/style.scss')
-    .pipe(scss({ outputStyle: 'compressed' }))
-    .pipe(concat('style.min.css'))
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['last 10 version'],
-      grid: true
-    }))
-    .pipe(dest('app/css'))
-    .pipe(browserSync.stream())
+   return src('app/scss/style.scss')
+      .pipe(scss({ outputStyle: 'compressed' }))
+      .pipe(concat('style.min.css'))
+      .pipe(
+         autoprefixer({
+            overrideBrowserslist: ['last 10 version'],
+            grid: true,
+         })
+      )
+      .pipe(dest('app/css'))
+      .pipe(browserSync.stream());
 }
 
 function build() {
-  return src([
-    'app/css/style.min.css',
-    'app/fonts/**/*',
-    'app/js/main.min.js',
-    'app/*.html'
-  ], { base: 'app' })
-    .pipe(dest('dist'))
+   return src(['app/css/style.min.css', 'app/fonts/**/*', 'app/js/main.min.js', 'app/*.html'], { base: 'app' }).pipe(dest('dist'));
 }
 
 function watching() {
-  watch(['app/scss/**/*.scss'], styles);
-  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-  watch(['app/*.html']).on('change', browserSync.reload);
+   watch(['app/scss/**/*.scss'], { usePolling: true }, styles);
+   watch(['app/js/**/*.js', { usePolling: true }, '!app/js/main.min.js'], scripts);
+   watch(['app/*.html']).on('change', browserSync.reload);
 }
 
 exports.styles = styles;
@@ -87,5 +76,3 @@ exports.cleanDist = cleanDist;
 
 exports.build = series(cleanDist, images, build);
 exports.default = parallel(styles, scripts, browsersync, watching);
-
-
